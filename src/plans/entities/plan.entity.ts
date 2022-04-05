f12913eb0a15e047101fdf1364fc720189a5fee6
seb-azot <sebastien.fourny@azot.dev>
2022-04-05 10:33:03 +0200

    ADD[scheam]ADD schema plan

--- src/plans/entities/plan.entity.ts
+++ src/plans/entities/plan.entity.ts
@@ -1,7 +1,18 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { UserResolver } from '../../user/user.resolver';
 
 @ObjectType()
 export class Plan {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID)
+  id?: string;
+  @Field()
+  description: string;
+  @Field()
+  image: string;
+  @Field()
+  url: string;
+  @Field()
+  createdAt: Date;
+  @Field()
+  author: string;
 }
